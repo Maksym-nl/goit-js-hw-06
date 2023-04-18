dd2639571d9336a3b0a5c5be085ee75fbbefe095
maksymnl <adw0988245131@gmail.com>
2023-04-18 07:27:09 +0300

    дз6
    дз6

---
+++ src/js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const ulEl = document.getElementById('ingredients');
+const liElements = ingredients.map((ingredient) => {
+let liEl = document.createElement('li')
+  liEl.textContent = ingredient;
+  liEl.classList.add('item');
+  return liEl;
+  
+})
+ulEl.append(...liElements);
+console.log(ulEl);
---
+++ src/js/task-09.js
@@ -0,0 +1,17 @@
+const body = document.body;
+const colorSpan = document.querySelector('.color');
+const btnColor = document.querySelector('.change-color');
+
+  btnColor.addEventListener("click", () => {
+    const color = getRandomHexColor();
+    
+  colorSpan.textContent = color;
+  body.style.backgroundColor = color;
+}
+);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
